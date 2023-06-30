141395175f2f15a883ca99ea577555956359b247
Oleksandra <none>
2023-06-30 22:30:24 +0200

    added event handler  deleteListItem, func lowerCaseFirstLetter

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-/* Задания на урок:
-
-1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
-новый фильм добавляется в список. Страница не должна перезагружаться.
-Новый фильм должен добавляться в movieDB.movies.
-Для получения доступа к значению input - обращаемся к нему как input.value;
-P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
-
-2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
-
-3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
-
-4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
-"Добавляем любимый фильм"
-
-5) Фильмы должны быть отсортированы по алфавиту */
-
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -32,12 +15,9 @@ document.addEventListener('DOMContentLoaded', () => {
         poster = document.querySelector('.promo__bg'),
         genre = poster.querySelector('.promo__genre'),
         movieList= document.querySelector('.promo__interactive-list'),
-        addForm = document.querySelector('.add'),
+        addForm = document.querySelector('form.add'),
         checkbox = addForm.querySelector('[type="checkbox"]'),
-        addInput = addForm.querySelector('.adding__input'), 
-        confirmButton = addForm.querySelector('button'),
-        deleteBtn = movieList.querySelectorAll('.delete');
-    
+        addInput = addForm.querySelector('.adding__input');
     
     const deleteAdv = (arr) => {
         arr.forEach( el => el.remove());
@@ -49,55 +29,53 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     const sortArr = arr => arr.sort(); 
+
+    const lowerCaseFirstLetter = str => {
+        return str[0].toLocaleLowerCase() + str.slice(1);
+    };
  
     const renderMovieDB = (films, parent) => {
         let lowerCaseMoviesArr = films.map(movie => movie.length < 21 ? 
-                                                    movie[0].toLocaleLowerCase() + movie.slice(1) :
-                                                    movie[0].toLocaleLowerCase() + movie.slice(1,20) + '...');
+                                                    lowerCaseFirstLetter(movie) :
+                                                    `${lowerCaseFirstLetter(movie).slice(0,20)}...`);
 
         sortArr(lowerCaseMoviesArr);
 
         parent.innerHTML = "";
+        sortArr(films);
 
         lowerCaseMoviesArr.forEach( (el, i) => {
             parent.innerHTML += `<li class="promo__interactive-item">${i + 1} ${el}
             <div class="delete"></div>
             </li>`;
         });
+
+        document.querySelectorAll('.delete').forEach((btn, i ) => {
+            btn.addEventListener('click', () => {
+                movieDB.movies.splice(i, 1);
+                renderMovieDB(movieDB.movies, movieList);
+            })
+        });
     };
 
-    deleteAdv(adv);
-    makeChanges();
-    sortArr(movieDB.movies);
-    renderMovieDB(movieDB.movies, movieList);
-    
-    function confirmButtonHandler(e) {
+    const confirmButtonHandler = e => {
         e.preventDefault();
         if (!addInput.value.trim()) return;
 
         if (checkbox.checked) {
             console.log("Добавляем любимый фильм");
         }
-        movieDB.movies.push(addInput.value);
+        movieDB.movies.push(lowerCaseFirstLetter(addInput.value));
+        movieDB.movies.sort();
         renderMovieDB(movieDB.movies, movieList);
-        addInput.value = '';
-    }
-    
-    confirmButton.addEventListener('click', confirmButtonHandler);
-    
-    // function deleteListItem(e){
-    //     console.log("    deleteeee    ");
-    // }
-    
-    // function testEvent(e){
-    //     console.log(e.target);
-    // }
-    
-    //deleteBtn.forEach( btn => btn.addEventListener('hover', () => console.log('click')));
-    
-    // for (let movie of deleteBtn){
-    //     //movie.addEventListener('click', testEvent);
-    //     movie.addEventListener('click', (e)=> {alert('hi!')} );
-    // }
-});
+        e.target.reset();
+        console.log(movieDB.movies)
+    };
 
+    deleteAdv(adv);
+    makeChanges();
+    movieDB.movies = movieDB.movies.map( movie => lowerCaseFirstLetter(movie));
+    renderMovieDB(movieDB.movies, movieList);
+    
+    addForm.addEventListener('submit', confirmButtonHandler);
+});
